bbe2631e8f85c11aab244f46f02b2aef5104ed4b
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-05-08 12:27:47 +0200

    Include Medium post link

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -11,7 +11,10 @@
       What I like
     </h1>
     <p>
-      Vue, Node, apollo, tailwindcss, WebStorm
+      Vue, Node
+    </p>
+    <p>
+      apollo, tailwindcss, WebStorm...
     </p>
 
     <h1 style="margin-top: 5rem;">
@@ -34,7 +37,35 @@
       What I write
     </h1>
     <p>
-      Medium post...
+      <a
+        href="https://medium.com/front-end-weekly/from-vuetify-to-tailwind-a7e83284ddfc"
+        target="_blank"
+        class="ced-link"
+      >
+        <div style="display: flex; align-items: center;">
+          <div style="width: 10px; margin-right: 0.7em;">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Layer_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 330 330"
+              style="enable-background: new 0 0 330 330;"
+              xml:space="preserve"
+            >
+              <path
+                id="XMLID_222_"
+                d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001  c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213  C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606  C255,161.018,253.42,157.202,250.606,154.389z"
+              />
+            </svg>
+          </div>
+          <div>
+            From Vuetify to tailwind
+          </div>
+        </div>
+      </a>
     </p>
 
     <!--    <h1 style="margin-top: 5rem;">-->
